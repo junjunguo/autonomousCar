c14e1754733f88d8bb20c0d760a88784864e2ad4
Junjun Guo <guojunjun@gmail.com>
2015-11-17 14:45:09 +0100

    eidt name list

--- js/init.js
+++ js/init.js
@@ -1,13 +1,21 @@
-(function($){
-  $(function(){
+(function ($) {
+    $(function () {
 
-    $('.button-collapse').sideNav();
-    $('.parallax').parallax();
+        $('.button-collapse').sideNav();
+        $('.parallax').parallax();
 
-  }); // end of document ready
+    }); // end of document ready
 })(jQuery); // end of jQuery name spac
 
 
-$(document).ready(function(){
-  $('.slider').slider({full_width: true});
+$(document).ready(function () {
+    $('.slider').slider({full_width: true});
 });// e
+
+
+var members = [
+    {selector: '#staggered-member', offset: 400, callback: 'Materialize.showStaggeredList("#staggered-member")'}
+
+];
+Materialize.scrollFire(members);
+
