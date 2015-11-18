eb1f2a4397f5de675b17c488c6329f9d40ea157b
Junjun Guo <guojunjun@gmail.com>
2015-11-18 21:36:42 +0100

    presentation 2

--- js/presentation.js
+++ js/presentation.js
@@ -3,6 +3,31 @@
  *
  */
 
-//$(document).ready(function(){
-//    $('.slider').slider({full_width: true});
+
+(function ($) {
+    $(function () {
+
+        $('.button-collapse').sideNav();
+        $('.parallax').parallax();
+        $('.slider').slider({auto: false, autoplay: false, interval: 6000000});
+        //
+        //// Pause slider
+        //$('.slider').slider('pause');
+        //// Start slider
+        //$('.slider').slider('start');
+        //// Next slide
+        //$('.slider').slider('next');
+        //// Previous slide
+        //$('.slider').slider('prev');
+
+
+    }); // end of document ready
+})(jQuery); // end of jQuery name spac
+
+//$(document).keydown(function (eventObject) {
+//    if (eventObject.which == 37) {//left arrow
+//        $('.slider .prev').click();//emulates click on prev button
+//    } else if (eventObject.which == 39) {//right arrow
+//        $('.slider .next').click();//emulates click on next button
+//    }
 //});
